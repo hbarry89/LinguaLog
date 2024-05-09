cbc68262df4c3c5fde697b3d329ee258bf03671b
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2024-05-08 20:27:51 -0400

    connect database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,22 @@
+// CREATE SERVER
 const express = require('express');
 const app = express();
+
+// CONNECT TO DATABASE
+const mongoose = require('mongoose');
+mongoose.connect();
+
+// IMPORT MODELS
+const UserModel = require('./models/Users');
+
+app.get('/', (req, res) => {
+    res.send('Welcome to LinguaLog!');
+});
+
+app.get('/users', async (req, res) => {
+    const users = await UserModel.find();
+    res.json(users);
+});
 
 app.listen('3001', () => {
   console.log('Server is running on port 3001');
